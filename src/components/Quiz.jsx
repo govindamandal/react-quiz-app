fb27ea68e0f9fac0e4060785adee852c4e77fe6e
Govinda Mandal <govinda4india@gmail.com>
2024-12-24 00:10:38 +0530

    Added Quiz component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
+import QUESTIONS from "../data/questions.js";
 
 function Quiz() {
-  return (
-    <div>Quiz</div>
-  )
+    const [answers, setAnswers] = useState([]);
+    const currentQuestionIndex = answers.length;
+    function handleSelectAnswer(selectedAnswer) {
+        setAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
+    }
+  return <div id="quiz">
+    <div id="question">
+    <h2>{QUESTIONS[currentQuestionIndex].text}</h2>
+    <ul id="answers">
+      {QUESTIONS[currentQuestionIndex].answers.map((answer, index) => (
+        <li key={index} className="answer">
+          <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+        </li>
+      ))}
+    </ul>
+  </div>
+  </div>;
 }
 
-export default Quiz
+export default Quiz;
